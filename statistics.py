f3b955b1b45a05a903751124ac06b22439c5134a
Suneel Varma <49975815+Suneelvarma5fi@users.noreply.github.com>
2022-10-13 18:15:19 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,3 +1,3 @@
 import math as m
 def calculateStats(numbers):
-  return dict({"avg": sum(numbers)/len(numbers), "min": min(numbers), "max": max(numbers)}) if numbers return dict({"avg": m.nan, "min": m.nan, "max": m.nan})
+  return dict({"avg": sum(numbers)/len(numbers), "min": min(numbers), "max": max(numbers)}) if numbers else dict({"avg": m.nan, "min": m.nan, "max": m.nan})
